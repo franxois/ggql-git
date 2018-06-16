2971ce84d27ec97e629d77cdec8b87f45ef7c73b
François Rieucau <franxoois@gmail.com>
2018-06-16 15:03:54 +0200

    feat(apollo): Add graphql querie in front

---
+++ front-vue-apollo/src/components/ProjectsList.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="apollo-example">
+    <!-- Apollo watched Graphql query -->
+    <ApolloQuery
+      :query="require('../graphql/ProjectList.gql')"
+    >
+      <template slot-scope="{ result: { loading, error, data } }">
+        <!-- Loading -->
+        <div v-if="loading" class="loading apollo">Loading...</div>
+
+        <!-- Error -->
+        <div v-else-if="error" class="error apollo">An error occured</div>
+
+        <!-- Result -->
+        <div v-else-if="data" class="result apollo">{{ JSON.stringify( data ) }}</div>
+
+        <!-- No result -->
+        <div v-else class="no-result apollo">No result :(</div>
+      </template>
+    </ApolloQuery>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.form,
+.input,
+.apollo,
+.message {
+  padding: 12px;
+}
+
+.input {
+  font-family: inherit;
+  font-size: inherit;
+  border: solid 2px #ccc;
+  border-radius: 3px;
+}
+
+.error {
+  color: red;
+}
+</style>
--- front-vue-apollo/src/views/Home.vue
+++ front-vue-apollo/src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ProjectsList/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import ProjectsList from "@/components/ProjectsList.vue";
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    ProjectsList
   }
 };
 </script>
