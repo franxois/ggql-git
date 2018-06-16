c36c621d3cbd6d4f1f4b843de64fa46d932462c8
François Rieucau <franxoois@gmail.com>
2018-06-16 15:01:42 +0200

    feat(CORS): enable CORS on backend

--- server/main.go
+++ server/main.go
@@ -6,14 +6,25 @@ import (
 	"net/http"
 
 	"github.com/franxois/ggql-git/server/graph"
+	"github.com/rs/cors"
 	"github.com/vektah/gqlgen/handler"
 )
 
 func main() {
 	app := &graph.MyApp{}
-	http.Handle("/", handler.Playground("Projects", "/graphql"))
-	http.Handle("/graphql", handler.GraphQL(graph.MakeExecutableSchema(app)))
+
+	mux := http.NewServeMux()
+	mux.Handle("/", handler.Playground("Projects", "/graphql"))
+	mux.Handle("/graphql", handler.GraphQL(graph.MakeExecutableSchema(app)))
+
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost", "http://localhost:8080", "http://localhost:8081"},
+		AllowCredentials: true,
+		AllowedHeaders:   []string{"Authorization", "Content-Type"},
+		// Enable Debugging for testing, consider disabling in production
+		Debug: true,
+	})
 
 	fmt.Println("Listening on :8081")
-	log.Fatal(http.ListenAndServe(":8081", nil))
+	log.Fatal(http.ListenAndServe(":8081", c.Handler(mux)))
 }
