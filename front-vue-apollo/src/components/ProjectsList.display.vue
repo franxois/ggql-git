ff1216ce0cef54afc716fd555b2e5fe887b2c6b5
François Rieucau <franxoois@gmail.com>
2018-06-30 17:20:28 +0200

    feat(vue): Display projects list in table

---
+++ front-vue-apollo/src/components/ProjectsList.display.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <table>
+      <thead><th>Name</th><th>Branch</th></thead>
+      <tbody>
+        <tr v-for="(project, idx) in data.projects" :key="idx" >
+          <td>{{ project.name }}</td>
+          <td>{{ project.currentBranch }}</td>
+        </tr>
+      </tbody>
+      </table>
+    <p>{{ JSON.stringify(data) }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    data: Object
+  }
+};
+</script>
